11fb569f99d1f9c76f6582def8c29516fafaf344
ale vel <ale.vrs@outlook.com>
2023-09-30 19:42:13 -0500

    Correct horizontal scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-/* import "./stylesheets/App.css";
-import freeCodeCampLogo from "./images/freecodecamp-logo.png";
-import { ListaDeTareas } from "./components/TasksList";
-
-function App() {
-  return (
-    <div className="aplicacion-tareas">
-      <div className="freecodecamp-logo-contenedor">
-        <img
-          className="freecodecamp-logo"
-          src={freeCodeCampLogo}
-          alt="logo freecodecamp"
-        />
-      </div>
-      <div className="tareas-lista-principal">
-        <h1>Mis Tareas</h1>
-        <ListaDeTareas />
-      </div>
-    </div>
-  );
-}
-
-export { App }; */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./stylesheets/App.css";
